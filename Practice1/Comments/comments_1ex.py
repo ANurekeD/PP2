cfa2e520d99aae4a47f4f9c963403d5baee71e41
ANurekeD <Smartnurzhan@gmail.com>
2026-09-14 23:03:11 +0500

    Castings

---
+++ Practice1/Comments/comments_1ex.py
@@ -0,0 +1,7 @@
+#wow,is this a comment?
+print("I learned how to use comments in python")
+
+print("Hello") #print("this is a comment,so it is not gonna print")
+
+#print("Hello,World")
+print("Congratulon, ma drilla")
